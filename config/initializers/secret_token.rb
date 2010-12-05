5effe5bb4c54903d4a995c6edd0c194c30d920c0
Nick <nick@laptopper.(none)>
2010-12-05 16:29:38 -0600

    First crack at holiday music playlist.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoHoHoHo::Application.config.secret_token = '3de44b8e449df1022692f21261e616ed598640f0292a57eb4e5f9d72163ca636533764cc43c6666774e4676347786de53fed30ebc8596a546a64f9ce71cc23d3'
